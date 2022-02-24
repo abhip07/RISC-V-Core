e72f08e3b71a36a6f695d0ea7335186eb3e1f8fe
abhip07 <65076876+abhip07@users.noreply.github.com>
2022-02-24 22:02:46 +0530

    Made changes in macro namings

--- core/constants.v
+++ core/constants.v
@@ -4,23 +4,23 @@
 
 */
 
-// Constants for ALU operations
+// Macros for different ALU operations
 
-`define ADD		4'd0
-`define SUB		4'd1
-`define AND		4'd2
-`define OR		4'd3
-`define XOR		4'd4
-`define MUL		4'd5
-`define DIV		4'd6
-`define DIVU	4'd7
-`define REM		4'd10
-`define REMU	4'd11
-`define SLL		4'd8
-`define SLTU  4'd9
-`define SRL		4'd14
-`define SRA		4'd15
-`define LUI		4'd12
-`define NONE	4'd13
+`define ADD		7'd0
+`define SUB		7'd1
+`define AND		7'd2
+`define OR		7'd3
+`define XOR		7'd4
+`define MUL		7'd5
+`define DIV		7'd6
+`define DIVU	7'd7
+`define REM		7'd10
+`define REMU	7'd11
+`define SLL		7'd8
+`define SLTU    7'd9
+`define SRL		7'd14
+`define SRA		7'd15
+`define LUI		7'd12
+`define NONE	7'd13
 
 
